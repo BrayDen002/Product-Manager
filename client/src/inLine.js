e527085803f8bf519eae5039534f8e47a92ad082
bdalit <70769843+bdalit@users.noreply.github.com>
2023-03-01 19:51:51 -0800

    Create inLine.js

---
+++ client/src/inLine.js
@@ -0,0 +1,15 @@
+ import React from 'react'
+
+ const heading = {
+
+ }
+
+ function inLine() {
+    return (
+        <div>
+            <h1 style={heading}> inLine </h1>
+        </div>
+    )
+ }
+
+ export default inLine
